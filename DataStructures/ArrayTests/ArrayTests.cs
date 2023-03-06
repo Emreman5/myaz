5c67979b71188f7b28ba1128aa69868688c26917
zcomert <zafer.comert@hotmail.com>
2023-03-06 16:54:09 +0300

    Test has been updated.

--- DataStructures/ArrayTests/ArrayTests.cs
+++ DataStructures/ArrayTests/ArrayTests.cs
@@ -1,5 +1,6 @@
 namespace ArrayTests
 {
+    // Unit Test
     public class ArrayTests
     {
         [Fact]
@@ -9,12 +10,48 @@ namespace ArrayTests
             var array = new Array.Array();
             array.Add("Ahmet");
             array.Add("Mehmet");
+            array.Add("Can");
 
             // Act
             int count = array.Count;
 
             // Assertion
-            Assert.Equal(2, count);
+            Assert.Equal(3, count);
+            Assert.Equal(4, array.Capacity);
+        }
+
+        [Fact]
+        public void Array_Add_Test()
+        {
+            // Arrange
+            var array = new Array.Array();
+            array.Add("Ahmet");
+            array.Add("Mehmet");
+            array.Add("Can");
+            array.Add("Canan");
+            array.Add("Filiz");
+
+            // Act
+            int count = array.Count;
+
+            // Assertion
+            Assert.Equal(5, count);
+            Assert.Equal(8, array.Capacity);
+        }
+
+        [Fact]
+        public void Array_GetItem_Test()
+        {
+            // Arrange
+            var array = new Array.Array();
+            array.Add("Ahmet");
+            array.Add("Mehmet");
+
+            // Act
+            var item = array.GetItem(1);
+
+            // Assert
+            Assert.Equal(item, "Mehmet");
         }
     }
 }

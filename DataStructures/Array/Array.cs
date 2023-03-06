a754f75bb27571d473aa9a31d89e457c3c595517
zcomert <zafer.comert@hotmail.com>
2023-03-06 15:15:11 +0300

    ArrayTests have been updated.

---
+++ DataStructures/Array/Array.cs
@@ -0,0 +1,19 @@
+﻿namespace Array;
+public class Array
+{
+    // Object
+    // Type : Array
+    private Object[] _InnerArray;
+    private int index = 0;
+    public int Count => _InnerArray.Length;  // Dizi kaç eleman var?
+    public Array()
+    {
+        _InnerArray = new Object[4]; // Block allocation
+    }
+    public void Add(Object item)
+    {
+        _InnerArray[index] = item;
+        index++;
+    }
+
+}

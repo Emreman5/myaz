0fdc8531477e7435e21e834383c7110cf26c48dd
zcomert <zafer.comert@hotmail.com>
2023-05-15 08:20:09 +0300

    Updated.

--- DataStructuresAlgorithms/SortingAlgorithmsApp/Program.cs
+++ DataStructuresAlgorithms/SortingAlgorithmsApp/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Secim yap:");
+Console.WriteLine("1. buble sort:");
+Console.WriteLine("2. Quick Sort");
+Console.WriteLine("3. ... Sort");
+
+// tercih 
+
+// 
